9353ae297c35d27108fa5f11eefd0a344bc38d49
pawan1222 <pawansingh6388251426@gmail.com>
2025-06-20 10:02:00 +0530

    updated

---
+++ javascript/file1.js
@@ -0,0 +1,17 @@
+// introduction to js
+let a=10
+let b="john"
+let age = 29
+let score
+let prompt = true
+let city=null
+console.log(a)
+console.log(typeof score)
+console.log(typeof city)
+
+
+let strage=String(age)
+console.log(typeof strage)
+let num="5"
+let strnum=Number(num)
+console.log(strnum)
---
+++ javascript/file2.js
@@ -0,0 +1,41 @@
+// operators in js
+
+// let a="10"
+// let b=10
+// console.log(a+b)
+// console.log(a*b)
+
+// let a=2.1
+// let b=2.1
+// console.log(a==b)
+
+// let a="2.1"
+// let b=2.1
+// console.log(a==b)
+
+// let a="2.1"
+// let b=2.1
+// console.log(a===b)
+
+// let a="2.1"
+// let b=2.1
+// console.log(a!==b)
+// let a="2.1"
+// let b=2.1
+// console.log(a!=b)
+
+
+// let a=10
+// let b=2
+// let c=12
+
+// console.log(a>b && a>c)
+
+
+let a=10
+let b=2
+let c=12
+
+console.log(a>b || a>c)
+console.log(!a>b)
+console.log(a**b)
